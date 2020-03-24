8ef74c07e6c313e61964bcd36176835eec50d337
rathorology <you@example.com>
2020-03-24 16:50:26 +0530

    its working

--- bert_summarizer.py
+++ bert_summarizer.py
@@ -1,4 +1,4 @@
-# import ast
+import ast
 #
 # import pandas as pd
 #
@@ -13,26 +13,28 @@
 #     a = pd.DataFrame(ast.literal_eval(i))['name'].tolist()
 #     f.append(a)
 # df['Features'] = f
-#
-
-
+# df.to_csv("finanical_risk_management.csv",index=False)
+########################################################################################################################
+import pandas as pd
 import tensorflow_hub as hub
 import tensorflow as tf
 import bert
-import tokenizer
+import numpy as np
+import os
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 FullTokenizer = bert.bert_tokenization.FullTokenizer
 from tensorflow.keras.models import Model  # Keras is the new high level API for TensorFlow
 import math
 
-max_seq_length = 128  # Your choice here.
+max_seq_length = 200  # Your choice here.
 input_word_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
                                        name="input_word_ids")
 input_mask = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
                                    name="input_mask")
 segment_ids = tf.keras.layers.Input(shape=(max_seq_length,), dtype=tf.int32,
                                     name="segment_ids")
-bert_layer = hub.KerasLayer("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/1",
+bert_layer = hub.KerasLayer("/home/rathorology/PycharmProjects/Text-summarizer/1",
                             trainable=True)
 pooled_output, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
 
@@ -70,23 +72,18 @@ vocab_file = bert_layer.resolved_object.vocab_file.asset_path.numpy()
 do_lower_case = bert_layer.resolved_object.do_lower_case.numpy()
 tokenizer = FullTokenizer(vocab_file, do_lower_case)
 
-dummy_sentance = "This is a nice sentence."
+df = pd.read_csv("/home/rathorology/PycharmProjects/Text-summarizer/finanical_risk_management.csv")
+dummy_sentance = df['review'][0]
+
 stokens = tokenizer.tokenize(dummy_sentance)
 stokens = ["[CLS]"] + stokens + ["[SEP]"]
 
 input_ids = get_ids(stokens, tokenizer, max_seq_length)
 input_masks = get_masks(stokens, max_seq_length)
 input_segments = get_segments(stokens, max_seq_length)
-print(stokens)
-print(input_ids)
-print(input_masks)
-print(input_segments)
-import numpy as np
-
 input_ids = np.array(input_ids)
 input_masks = np.array(input_masks)
 input_segments = np.array(input_segments)
-
 pool_embs, all_embs = model.predict([[input_ids], [input_masks], [input_segments]])
 
 
@@ -100,4 +97,17 @@ def cosine_similarity(x, y):
     return numerator / float(denominator)
 
 
-print(cosine_similarity(pool_embs[0], all_embs[0][0]))
+for dummy_feature in ast.literal_eval(df['Features'][0]):
+    f_stokens = tokenizer.tokenize(dummy_feature)
+    feat_stokens = ["[CLS]"] + f_stokens + ["[SEP]"]
+
+    f_input_ids = get_ids(feat_stokens, tokenizer, max_seq_length)
+    f_input_masks = get_masks(feat_stokens, max_seq_length)
+    f_input_segments = get_segments(feat_stokens, max_seq_length)
+
+    f_input_ids = np.array(f_input_ids)
+    f_input_masks = np.array(f_input_masks)
+    f_input_segments = np.array(f_input_segments)
+    f_pool_embs, f_all_embs = model.predict([[f_input_ids], [f_input_masks], [f_input_segments]])
+
+    print(str(dummy_feature), cosine_similarity(all_embs[0][0], f_all_embs[0][0]))
